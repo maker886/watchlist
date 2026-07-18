581ea8033cceb0b2fe2233a6f0fc4be0eb6ed61d
maker <guoshaodong666@gmail.com>
2026-07-18 22:25:00 +0800

    Add database support with Flask-SQLAlchemy

--- app.py
+++ app.py
@@ -2,7 +2,42 @@ from flask import Flask
 from markupsafe import escape
 from flask import url_for
 from flask import render_template
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
+from pathlib import Path
+import os
+import sys
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
+import click
+
+SQLITE_PREFIX = 'sqlite:///' if sys.platform.startswith('win') else 'sqlite:////'
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = SQLITE_PREFIX + str(Path(app.root_path) / 'data.db')
+
+class Base(DeclarativeBase):
+    pass
+
+db=SQLAlchemy(app,model_class=Base)
+class User(db.Model):
+    __tablename__ = 'user' # 定义表名称
+    id: Mapped[int] = mapped_column(primary_key=True)  # 主键
+    name: Mapped[str] = mapped_column(String(20))  # 名字
+
+
+class Movie(db.Model):  # 表名将会是 movie
+    __tablename__ = 'movie'
+    id: Mapped[int] = mapped_column(primary_key=True)  # 主键
+    title: Mapped[str] = mapped_column(String(60))  # 电影标题
+    year: Mapped[str] = mapped_column(String(4))  # 电影年份
+@app.cli.command('init-db')  # 注册为命令，传入自定义命令名
+@click.option('--drop', is_flag=True, help='Create after drop.')  # 设置选项
+def init_database(drop):
+    """Initialize the database."""
+    if drop:  # 判断是否输入了选项
+        db.drop_all()
+    db.create_all()
+    click.echo('Initialized database.')  # 输出提示信息
 @app.route('/index')
 @app.route('/home')
 
@@ -28,21 +63,43 @@ def test_url_for():
     print(url_for('test_url_for', num=2))  # 输出：/test?num=2
     return 'Test page'
 
-name = 'maker'
-movies = [
-    {'title': 'My Neighbor Totoro', 'year': '1988'},
-    {'title': 'Dead Poets Society', 'year': '1989'},
-    {'title': 'A Perfect World', 'year': '1993'},
-    {'title': 'Leon', 'year': '1994'},
-    {'title': 'Mahjong', 'year': '1996'},
-    {'title': 'Swallowtail Butterfly', 'year': '1996'},
-    {'title': 'King of Comedy', 'year': '1999'},
-    {'title': 'Devils on the Doorstep', 'year': '1999'},
-    {'title': 'WALL-E', 'year': '2008'},
-    {'title': 'The Pork of Music', 'year': '2012'},
-]
+import click
+
+
+@app.cli.command()
+def forge():
+    """Generate fake data."""
+    db.drop_all()
+    db.create_all()
+
+    # 全局的两个变量移动到这个函数内
+    name = 'maker'
+    movies = [
+        {'title': 'My Neighbor Totoro', 'year': '1988'},
+        {'title': 'Dead Poets Society', 'year': '1989'},
+        {'title': 'A Perfect World', 'year': '1993'},
+        {'title': 'Leon', 'year': '1994'},
+        {'title': 'Mahjong', 'year': '1996'},
+        {'title': 'Swallowtail Butterfly', 'year': '1996'},
+        {'title': 'King of Comedy', 'year': '1999'},
+        {'title': 'Devils on the Doorstep', 'year': '1999'},
+        {'title': 'WALL-E', 'year': '2008'},
+        {'title': 'The Pork of Music', 'year': '2012'},
+    ]
+
+    user = User(name=name)
+    db.session.add(user)
+    for m in movies:
+        movie = Movie(title=m['title'], year=m['year'])
+        db.session.add(movie)
+
+    db.session.commit()
+    click.echo('Done.')
 @app.route('/')
 def index():
-    return render_template('index.html', name=name, movies=movies)
+    user = db.session.execute(select(User)).scalar()  # 读取用户记录
+    movies = db.session.execute(select(Movie)).scalars().all()  # 读取所有电影记录
+    return render_template('index.html', user=user, movies=movies)
 if __name__ == '__main__':
     app.run(debug=True)
+
