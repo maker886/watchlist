07d475e219c8c289e84e55ddde1425274ad28269
maker <guoshaodong666@gmail.com>
2026-07-18 21:25:52 +0800

    Add static files

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask
 from markupsafe import escape
 from flask import url_for
+from flask import render_template
 app = Flask(__name__)
-@app.route('/')
 @app.route('/index')
 @app.route('/home')
 
@@ -28,5 +28,21 @@ def test_url_for():
     print(url_for('test_url_for', num=2))  # 输出：/test?num=2
     return 'Test page'
 
+name = 'maker'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
+@app.route('/')
+def index():
+    return render_template('index.html', name=name, movies=movies)
 if __name__ == '__main__':
     app.run(debug=True)
